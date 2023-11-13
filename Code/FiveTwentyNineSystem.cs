639609144684efe44d5e64aa91c009f308ffbe66
algernon-A <aeneas@aloku.net>
2023-11-13 16:39:28 +1100

    Convert MilestoneJob to IJobEntity.

--- Code/FiveTwentyNineSystem.cs
+++ Code/FiveTwentyNineSystem.cs
@@ -4,18 +4,14 @@
 
 namespace FiveTwentyNineTiles
 {
-    using System.Linq;
-    using Colossal.Collections;
     using Colossal.Serialization.Entities;
     using Game;
     using Game.Areas;
     using Game.Common;
     using Game.Prefabs;
     using Game.Serialization;
-    using Unity.Burst.Intrinsics;
     using Unity.Collections;
     using Unity.Entities;
-    using Unity.Jobs;
 
     /// <summary>
     /// The 529 tile mod system.
@@ -69,7 +65,7 @@ namespace FiveTwentyNineTiles
         /// <summary>
         /// Job to reassign milestone tile counts.
         /// </summary>
-        public struct MilestoneJob : IJobChunk
+        public partial struct MilestoneJob : IJobEntity
         {
             /// <summary>
             /// Data type handle (<see cref="MilestoneData"/>).
@@ -79,84 +75,71 @@ namespace FiveTwentyNineTiles
             /// <summary>
             /// Job execution.
             /// </summary>
-            /// <param name="chunk"><see cref="Chunk"/> containing entities.</param>
-            /// <param name="unfilteredChunkIndex">The index of the current <see cref="Chunk"/> within the list of all chunks in all archetypes matched by the <see cref="EntityQuery"/> that the job was run against.</param>
-            /// <param name="useEnabledMask">A value indicating whether <c>chunkEnabledMask</c> should be used to filter the provided <see cref="EntityQuery"/>.</param>
-            /// <param name="chunkEnabledMask">A mask to filter the provided <see cref="EntityQuery"/>.  Ignored if <c>useEnabledMask</c> is <c>false</c>.</param>
-            public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+            /// <param name="milestone"><see cref="MilestoneData"/> component.</param>
+            public void Execute(ref MilestoneData milestone)
             {
-                // Iterate through each MilestoneData component in this chunk and update the tile count.
-                NativeArray<MilestoneData> milestoneArray = chunk.GetNativeArray(ref m_MilestoneDataType);
-                for (int i = 0; i < chunk.Count; i++)
+                switch (milestone.m_MapTiles)
                 {
-                    ref MilestoneData reference = ref milestoneArray.ElementAt(i);
-                    int newTileCount = reference.m_MapTiles;
-
-                    switch (newTileCount)
-                    {
-                        case 3:
-                            newTileCount = 4;
-                            break;
-                        case 4:
-                            newTileCount = 5;
-                            break;
-                        case 5:
-                            newTileCount = 7;
-                            break;
-                        case 6:
-                            newTileCount = 7;
-                            break;
-                        case 7:
-                            newTileCount = 8;
-                            break;
-                        case 8:
-                            newTileCount = 10;
-                            break;
-                        case 9:
-                            newTileCount = 11;
-                            break;
-                        case 10:
-                            newTileCount = 12;
-                            break;
-                        case 12:
-                            newTileCount = 14;
-                            break;
-                        case 15:
-                            newTileCount = 18;
-                            break;
-                        case 18:
-                            newTileCount = 22;
-                            break;
-                        case 21:
-                            newTileCount = 25;
-                            break;
-                        case 24:
-                            newTileCount = 29;
-                            break;
-                        case 28:
-                            newTileCount = 34;
-                            break;
-                        case 32:
-                            newTileCount = 38;
-                            break;
-                        case 36:
-                            newTileCount = 43;
-                            break;
-                        case 41:
-                            newTileCount = 49;
-                            break;
-                        case 46:
-                            newTileCount = 55;
-                            break;
-                        case 51:
-                            newTileCount = 61;
-                            break;
-                        case 56:
-                            newTileCount = 68;
-                            break;
-                    }
-
-                    reference.m_MapTiles = newTileCount;
+                    case 3:
+                        milestone.m_MapTiles = 4;
+                        break;
+                    case 4:
+                        milestone.m_MapTiles = 5;
+                        break;
+                    case 5:
+                        milestone.m_MapTiles = 7;
+                        break;
+                    case 6:
+                        milestone.m_MapTiles = 7;
+                        break;
+                    case 7:
+                        milestone.m_MapTiles = 8;
+                        break;
+                    case 8:
+                        milestone.m_MapTiles = 10;
+                        break;
+                    case 9:
+                        milestone.m_MapTiles = 11;
+                        break;
+                    case 10:
+                        milestone.m_MapTiles = 12;
+                        break;
+                    case 12:
+                        milestone.m_MapTiles = 14;
+                        break;
+                    case 15:
+                        milestone.m_MapTiles = 18;
+                        break;
+                    case 18:
+                        milestone.m_MapTiles = 22;
+                        break;
+                    case 21:
+                        milestone.m_MapTiles = 25;
+                        break;
+                    case 24:
+                        milestone.m_MapTiles = 29;
+                        break;
+                    case 28:
+                        milestone.m_MapTiles = 34;
+                        break;
+                    case 32:
+                        milestone.m_MapTiles = 38;
+                        break;
+                    case 36:
+                        milestone.m_MapTiles = 43;
+                        break;
+                    case 41:
+                        milestone.m_MapTiles = 49;
+                        break;
+                    case 46:
+                        milestone.m_MapTiles = 55;
+                        break;
+                    case 51:
+                        milestone.m_MapTiles = 61;
+                        break;
+                    case 56:
+                        milestone.m_MapTiles = 68;
+                        break;
                 }
             }
         }
